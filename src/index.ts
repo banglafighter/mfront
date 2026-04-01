1ce6372f9f9c8cdb379e9e627af19e1776eb426f
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-01 22:55:07 +0600

    Working on project structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import MRoute from "./router/mroute"
-import { PageSlot } from "./router/mroute-common"
+import {PageSlot} from "./router/mroute-common"
 import {LayoutComponent, PageComponent, RouteData, RoutePage} from "./router/mroute-data"
 import {useRouteRequest} from "./router/use-mroute-request"
-
+import {
+    mmReactUseContext as useContext,
+    mmReactUseState as useState,
+    mmReactUseRef as useRef
+} from "mmcore";
 
 export type {
     PageComponent,
@@ -15,4 +19,7 @@ export {
     MRoute,
     useRouteRequest,
     PageSlot,
+    useContext,
+    useState,
+    useRef,
 }
