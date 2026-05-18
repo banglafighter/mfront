adc876b0a2536fa33978fd3d07e35f6726c1a628
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-19 00:10:42 +0600

    Working on release

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,5 +4,12 @@ import {baseConfig} from '../../tsdown.base.mjs'
 export default defineConfig({
     ...baseConfig,
     entry: ['src/index.ts'],
-    outDir: 'dist'
+    outDir: 'dist',
+    deps: {
+        alwaysBundle: [
+            "i18next",
+            "i18next-browser-languagedetector",
+            "react-i18next"
+        ]
+    }
 })
