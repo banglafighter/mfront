1cd67a35066dd7596b337fe489caa3ff97c37a73
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-20 09:08:35 +0600

    Working on Release

--- src/engine/mpage-engine.tsx
+++ src/engine/mpage-engine.tsx
@@ -2,11 +2,9 @@ import MRegistry from "../structure/mregistry";
 import {UIAdapterProvider} from "mfront-ui";
 import {MmReactSuspense} from "mmcore";
 import {McRouterProvider} from "mfront-core";
-import {useI18n} from "./../libsfea/mf-i18n";
 
 
 export default function MPageEngine({registry}: { registry: MRegistry }) {
-    useI18n();
     return (
         <>
             <UIAdapterProvider adapter={registry.adapter.setUIAdapter()}>
