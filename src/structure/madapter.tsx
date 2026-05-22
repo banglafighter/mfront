2f4e3f19047c3ae90b2e560c1b51315028c48d85
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-23 04:45:17 +0600

    Added new dependency

--- src/structure/madapter.tsx
+++ src/structure/madapter.tsx
@@ -1,8 +1,10 @@
 import {UINode} from "mmcore";
 import {UIAdapterData} from "mfront-ui";
+import {HTTPClient} from "mfront-core";
 
 export default abstract class MAdapter {
     abstract setCentralUI(): UINode
     abstract setSuspense(): UINode
     abstract setUIAdapter(): UIAdapterData
+    abstract setHTTPClient(): HTTPClient
 }
